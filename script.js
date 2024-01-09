fac2488fa011eb6f15a43a1c3ca7ee34f291f1d1
Manas Sharma <137026362+Manas5353@users.noreply.github.com>
2024-01-09 17:50:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const body = document.body;
 let lastScroll = 0;
 
-window.addEventListener('scroll', () =>{
-    const currScroll =window.scrollX
-    if(currScroll <=0){
-        body.classList.remove("scroll-up")
-    }
+window.addEventListener('scroll', () => {
+  const currScroll = window.scrollX;
 
-    if(currScroll > lastScroll && !body.classList.contains("scroll-down")){
-        body.classList.remove("scroll-up")
-        body.classList.add("scroll-down")
-    }
-    if(currScroll < lastScroll && !body.classList.contains("scroll-down")){
-        body.classList.remove("scroll-down")
-        body.classList.add("scroll-up")
-    }
-    lastScroll = currScroll;
-})
+  if (currScroll <= 0) {
+    body.removeAttribute("id");
+  }
+
+  if (currScroll > lastScroll && body.id !== "scroll-down") {
+    body.removeAttribute("id");
+    body.setAttribute("id", "scroll-down");
+  }
+
+  if (currScroll < lastScroll && body.id !== "scroll-up") {
+    body.removeAttribute("id");
+    body.setAttribute("id", "scroll-up");
+  }
+
+  lastScroll = currScroll;
+});
